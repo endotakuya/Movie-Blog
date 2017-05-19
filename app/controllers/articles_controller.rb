0179ed787dd91f44e30e6739e8e6b7639bec9f64
endotakuya <endo.takuya.0701@gmail.com>
2017-05-19 17:10:56 +0900

    リファクタリング、Articleのdebug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,8 @@ class ArticlesController < ApplicationController
 
   def show
     # １記事のデータを取得
-    @article = Article.find(params[:id])
+    movie_id = params[:id]
+    @article = Article.find(movie_id)
     # 映画情報取得
     access_tmdb
     @movie = Tmdb::Movie.detail(@article[:movie_id])
@@ -23,10 +24,9 @@ class ArticlesController < ApplicationController
 
   def create
     @article = current_user.articles.build(article_params)
-
     if @article.save
       sweetalert_success('', '記事を投稿しました', timer: 2000 )
-      redirect_to movie_path(@article.movie_id)
+      redirect_to movie_url(@article.movie_id)
     else
       sweetalert_error('記入漏れがないか、確認してください','投稿できませんでした', persistent: 'OK')
       render :new
@@ -35,19 +35,17 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = Article.find(params[:id])
-
     # JSで値を使えるように
     gon.point_list = [ @article.point_1, @article.point_2, @article.point_3, @article.point_4, @article.point_5]
   end
 
   def update
     @article = Article.find(params[:id])
-
     if @article.update(article_params)
       sweetalert_success('', '更新しました', timer: 2000 )
       redirect_to @article
     else
-      sweetalert_error('記入漏れがないか、確認してください','更新できませんでした', persistent: 'OK')
+      sweetalert_error('記入漏れがないか、確認してください', '更新できませんでした', persistent: 'OK')
       render :edit
     end
   end
