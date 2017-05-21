196c52710f2ac3a6c52f248c253b1136129355be
enta0701 <endo.takuya.0701@gmail.com>
2017-05-22 00:16:23 +0900

    リファクタリング

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
     def text_url_to_link(text)
         URI.extract(text, ['http', 'https']).uniq.each do |url|
-            sub_text = ""
-            sub_text = "<a href=\"#{url}\" target=\"_blank\">#{url}</a>"
+            sub_text = link_to url, url,　target: '_blank'
             text.gsub!(url, sub_text)
         end
     text
