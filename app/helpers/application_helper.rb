09175e9db4375984ce5d8de1e5f1569ca21efa2c
endotakuya <endo.takuya.0701@gmail.com>
2017-05-22 21:42:17 +0900

    URLに_blankを追加@Chat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
+
+    def message_escape(message)
+        simple_format(text_url_to_link(h(message)).html_safe, {}, :sanitize => false)
+    end
+    
     def text_url_to_link(text)
         URI.extract(text, ['http', 'https']).uniq.each do |url|
-            sub_text = link_to url, url,　target: '_blank'
+            sub_text = link_to url, url, target: '_blank'
             text.gsub!(url, sub_text)
         end
     text
     end
+
 end
