189ceb5ebc584a11c9ec4347aaa3e5aa2d4edd79
endotakuya <endo.takuya.0701@gmail.com>
2017-05-28 05:53:56 +0900

    ・無効なURLからの侵入をカット@チャット ・ログインしたことのあるチャット一覧を、入ったことのある順番で表示

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,15 +3,10 @@ class RoomsController < ApplicationController
   layout 'room'
 
   def show
-    # RoomID を 映画ID と紐付け
     @room_id = params[:id]
-    current_user.join_room(@room_id)
-
-    # 現在のRoom内メッセージを取得
-    @messages = Message.all.where(room_id: @room_id)
 
     # 過去に入ったことのあるチャット一覧
-    room_ids = current_user.user_rooms.pluck(:room_id)
+    room_ids = current_user.user_rooms.order("updated_at DESC").pluck(:room_id)
 
     # 過去に入ったことのあるチャットと映画を紐付け
     @join_rooms = {}
@@ -25,12 +20,22 @@ class RoomsController < ApplicationController
     # メッセージ切り分け用
     @user_id = current_user.id
 
+    # RoomID を 映画ID と紐付け
+    current_user.join_room(@room_id)
+
+    # 現在のRoom内メッセージを取得
+    @messages = Message.all.where(room_id: @room_id)
+    
   end
 
   private
   def join_movie(room_id)
     access_tmdb
     movie = Tmdb::Movie.detail(room_id)
+    unless movie['status_code'].nil?
+      sweetalert_error(movie['status_message'],'チャットに入れませんでした', persistent: 'OK')
+      redirect_to root_url
+    end
     movie
   end
   
